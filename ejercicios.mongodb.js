20e24fc5dedf3bfe37694c3ef4fe28a291d02f39
sergioM <sermarpad@gmail.com>
2025-11-18 20:55:02 +0100

    hasta el 6

--- ejercicios.mongodb.js
+++ ejercicios.mongodb.js
@@ -9,7 +9,7 @@ db.animales.aggregate([
     },
   },
 ]);
-
+//2
 db.animales.aggregate([
     {$match:{
         edad: { $gt: 3},
@@ -22,4 +22,63 @@ db.animales.aggregate([
   }
 }
 ])
+
+//3
+
+db.animales.aggregate([{
+    $project: {
+      _id: 0,
+      nombre: 1,
+      raza: 1,
+      totalVacunas: { $size: "$historico_vacunas" },
+      vacunasMinimas: { $cond: {
+          if: {$eq: [{$size: "$historico_vacunas" }, 3]},
+          then: true,
+          else: false,
+        }}
+    }
+  },{
+    $match: {
+      vacunasMinimas: true
+    }
+  }
+])
+//4
+db.animales.aggregate([{
+    $match: {
+      consulta: "Le duele la pata derecha"
+    }
+    
+  },{$project:{ _id:0,nombre:1}}
+])
+
+//5
+db.animales.aggregate([{
+    $match: {
+      "historico_vacunas": {
+        $elemMatch: { tipo: "Parvovirus" }
+  }}},{
+    $project: {
+      _id: 0,
+      nombre: 1,
+      raza: 1,
+    }
+  }
+])
+
+//6
+db.animales.aggregate([{
+    $match: {
+      "historico_vacunas": {
+        $elemMatch: {
+          fechaVacuna: { $regex: "^2023" } }
+  }}},{
+    $project: {
+      _id: 0,
+      nombre: 1,
+      raza: 1,
+    }
+  }
+])
+
 
